8fb7cd29159d0204eb696a36c71b5088510aa1ef
Souhail Smiri <souhail.smiriaa@gmail.com>
2026-07-23 19:37:10 +0200

    Update skillsData.ts to add new skills and reorder existing ones

--- content/skillsData.ts
+++ content/skillsData.ts
@@ -1,33 +1,55 @@
 import { SkillType } from "@lib/types";
 import {
-  SiHtml5,
-  SiCss3,
   SiJavascript,
   SiNextdotjs,
   SiTailwindcss,
   SiPython,
-  SiMysql,
   SiCsharp,
   SiCplusplus,
   SiSupabase,
   SiKubernetes,
   SiUnity,
   SiLinux,
-  SiAndroidstudio,
   SiRaspberrypi,
+  SiDocker,
+  SiTypescript,
+  SiReact,
+  SiArduino,
+  SiPostgresql,
 } from "react-icons/si";
+import { SiTensorflow, SiOpencv } from "react-icons/si";
 
 const skills: SkillType[] = [
   {
-    name: "HTML",
-    Icon: SiHtml5,
+    name: "Python",
+    Icon: SiPython,
   },
   {
-    name: "CSS",
-    Icon: SiCss3,
+    name: "Computer Vision",
+    Icon: SiOpencv,
   },
   {
-    name: "Javascript",
+    name: "TensorFlow",
+    Icon: SiTensorflow,
+  },
+  {
+    name: "Unity",
+    Icon: SiUnity,
+  },
+  {
+    name: "C#",
+    Icon: SiCsharp,
+  },
+  {
+    name: "C++",
+    Icon: SiCplusplus,
+  },
+  {
+    name: "TypeScript",
+    Icon: SiTypescript,
+  },
+  {
+    name: "JavaScript",
     Icon: SiJavascript,
   },
   {
@@ -35,48 +57,40 @@ const skills: SkillType[] = [
     Icon: SiNextdotjs,
   },
   {
+    name: "React",
+    Icon: SiReact,
+  },
+  {
     name: "Tailwind CSS",
     Icon: SiTailwindcss,
-  },
-  {
-    name: "C++",
-    Icon: SiCplusplus,
-  },
-  {
-    name: "C#",
-    Icon: SiCsharp,
-  },
-  {
-    name: "Python",
-    Icon: SiPython,
-  },
-  {
-    name: "MySQL",
-    Icon: SiMysql,
-  },
-  {
-    name: "Android Studio",
-    Icon: SiAndroidstudio,
-  },
-  {
-    name: "Supabase",
-    Icon: SiSupabase,
-  },
-  {
-    name: "Unity",
-    Icon: SiUnity,
   },
   {
     name: "Kubernetes",
     Icon: SiKubernetes,
   },
   {
-    name: "Raspberry Pi",
-    Icon: SiRaspberrypi,
+    name: "Docker",
+    Icon: SiDocker,
   },
   {
     name: "Linux",
     Icon: SiLinux,
+  },
+  {
+    name: "Raspberry Pi",
+    Icon: SiRaspberrypi,
+  },
+  {
+    name: "Arduino",
+    Icon: SiArduino,
+  },
+  {
+    name: "PostgreSQL",
+    Icon: SiPostgresql,
+  },
+  {
+    name: "Supabase",
+    Icon: SiSupabase,
   },
 ];
 
